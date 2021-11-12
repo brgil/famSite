e29f3536e097dcaa03b3a37d8cfee399fd48f743
brgil <61500380+brgil@users.noreply.github.com>
2021-11-12 15:37:08 +0000

    separate js file for landing

---
+++ rosita/rosita.js
@@ -0,0 +1,14 @@
+
+
+function changeBg(background) {
+    document.body.style.backgroundImage = "url('./images/wallpapers/" + background + "')";
+
+}
+
+function bg(background) { /* DELETE THIS */
+    document.body.style.backgroundImage = "url(" + background + ")";
+
+}
+
+
+
