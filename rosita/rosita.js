3c2b572ce5c53b8dd87c5acf32adeace995f5e76
brgil <61500380+brgil@users.noreply.github.com>
2021-11-25 14:28:11 +0000

    Implemented Map

--- rosita/rosita.js
+++ rosita/rosita.js
@@ -1,9 +1,32 @@
 
 
 function changeBg(background) {
+
+
     document.body.style.backgroundImage = "url('./images/wallpapers/" + background + "')";
+    
+}
+
+function toMap() {
+
+    // document.querySelector(".travelbtn").addEventListener("click", function(){
+        document.body.style.backgroundImage = "";
+        document.querySelector(".mapsvg").style.display = "block";
+    //});
 
 }
+
+function travel(location) {
+    // essentially same as changeBg for now
+    document.querySelector(".mapsvg").style.display = "none";
+    document.body.style.backgroundImage = "url('./images/wallpapers/" + location + "')";
+    // !!! INSERT APPEAR ROSITA HERE !!!
+}
+
+
+
+
+
 
 function bg(background) { /* DELETE THIS */
     document.body.style.backgroundImage = "url(" + background + ")";
