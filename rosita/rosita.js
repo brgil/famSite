9394229436eefc38e6bc9154d95e76db5e46b459
brgil <61500380+brgil@users.noreply.github.com>
2021-11-25 18:03:56 +0000

    fix responsive map

--- rosita/rosita.js
+++ rosita/rosita.js
@@ -11,7 +11,7 @@ function toMap() {
 
     // document.querySelector(".travelbtn").addEventListener("click", function(){
         document.body.style.backgroundImage = "none";
-        document.querySelector(".mapsvg").style.display = "block";
+        document.querySelector(".mapSvg").style.display = "block";
         document.querySelector(".rosita").style.display = "none";
         document.querySelector(".toMapButton").style.display = "none";
     //});
@@ -20,7 +20,7 @@ function toMap() {
 
 function travel(location) {
     // essentially same as changeBg for now
-    document.querySelector(".mapsvg").style.display = "none";
+    document.querySelector(".mapSvg").style.display = "none";
     document.body.style.backgroundImage = "url('./images/wallpapers/" + location + "')";
     document.querySelector(".rosita").style.display = "block";
     document.querySelector(".toMapButton").style.display = "block";
