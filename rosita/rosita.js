83aa78bd080f522d1752087bf0d593fe295d3d2d
brgil <61500380+brgil@users.noreply.github.com>
2021-11-25 17:06:34 +0000

    Completed World Map

--- rosita/rosita.js
+++ rosita/rosita.js
@@ -10,8 +10,10 @@ function changeBg(background) {
 function toMap() {
 
     // document.querySelector(".travelbtn").addEventListener("click", function(){
-        document.body.style.backgroundImage = "";
+        document.body.style.backgroundImage = "none";
         document.querySelector(".mapsvg").style.display = "block";
+        document.querySelector(".rosita").style.display = "none";
+        document.querySelector(".toMapButton").style.display = "none";
     //});
 
 }
@@ -20,7 +22,8 @@ function travel(location) {
     // essentially same as changeBg for now
     document.querySelector(".mapsvg").style.display = "none";
     document.body.style.backgroundImage = "url('./images/wallpapers/" + location + "')";
-    // !!! INSERT APPEAR ROSITA HERE !!!
+    document.querySelector(".rosita").style.display = "block";
+    document.querySelector(".toMapButton").style.display = "block";
 }
 
 
