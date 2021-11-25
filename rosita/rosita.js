d7984172ff9340388d29710db3172c66148e9a59
brgil <61500380+brgil@users.noreply.github.com>
2021-11-25 18:24:15 +0000

    fix map

--- rosita/rosita.js
+++ rosita/rosita.js
@@ -1,25 +1,16 @@
 
 
-function changeBg(background) {
-
-
-    document.body.style.backgroundImage = "url('./images/wallpapers/" + background + "')";
-    
-}
-
 function toMap() {
 
-    // document.querySelector(".travelbtn").addEventListener("click", function(){
-        document.body.style.backgroundImage = "none";
-        document.querySelector(".mapSvg").style.display = "block";
-        document.querySelector(".rosita").style.display = "none";
-        document.querySelector(".toMapButton").style.display = "none";
-    //});
+    document.body.style.backgroundImage = "none";
+    document.querySelector(".mapSvg").style.display = "block";
+    document.querySelector(".rosita").style.display = "none";
+    document.querySelector(".toMapButton").style.display = "none";
 
 }
 
 function travel(location) {
-    // essentially same as changeBg for now
+
     document.querySelector(".mapSvg").style.display = "none";
     document.body.style.backgroundImage = "url('./images/wallpapers/" + location + "')";
     document.querySelector(".rosita").style.display = "block";
@@ -31,7 +22,7 @@ function travel(location) {
 
 
 
-function bg(background) { /* DELETE THIS */
+function bg(background) { /* for testing */
     document.body.style.backgroundImage = "url(" + background + ")";
 
 }
