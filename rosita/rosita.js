090d1deafecdda18f3e2a2c4eaf5cefca6591e39
brgil <61500380+brgil@users.noreply.github.com>
2023-01-02 12:57:01 +0000

    Renamed elements to generic

--- rosita/rosita.js
+++ rosita/rosita.js
@@ -1,28 +1,35 @@
+function goHome() {
 
+    document.body.style.backgroundImage = "none";
+    document.querySelector("#mapSvg").style.display = "none";
+    document.querySelector("#plushie").style.display = "block";
+    document.querySelector("#toMapButton").style.display = "block";
+
+}
 
 function toMap() {
 
     document.body.style.backgroundImage = "none";
-    document.querySelector(".mapSvg").style.display = "block";
-    document.querySelector(".rosita").style.display = "none";
-    document.querySelector(".toMapButton").style.display = "none";
+    document.querySelector("#mapSvg").style.display = "block";
+    document.querySelector("#plushie").style.display = "none";
+    document.querySelector("#toMapButton").style.display = "none";
 
 }
 
 function travel(location) {
 
-    document.querySelector(".mapSvg").style.display = "none";
+    document.querySelector("#mapSvg").style.display = "none";
     document.body.style.backgroundImage = "url('./images/wallpapers/" + location + "')";
-    document.querySelector(".rosita").style.display = "block";
-    document.querySelector(".toMapButton").style.display = "block";
+    document.querySelector("#plushie").style.display = "block";
+    document.querySelector("#toMapButton").style.display = "block";
 }
 
 
 
 
 
-
-function bg(background) { /* for testing */
+/* TESTING FUNCTIONS */
+function bg(background) { 
     document.body.style.backgroundImage = "url(" + background + ")";
 
 }
